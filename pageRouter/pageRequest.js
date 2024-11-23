abadcc0115edf75bd40388614ec8decad6e71836
Nguyễn Khoa <khoacode1305@gmail.com>
2024-11-23 12:27:27 -0700

    feat: create root page

--- pageRouter/pageRequest.js
+++ pageRouter/pageRequest.js
@@ -3,6 +3,9 @@ const query = require('./pageQueries');
 
 const pageRouter = express.Router();
 
+pageRouter.get('/', (req, res) => {
+    res.send("Hello World");
+})
 pageRouter.post('/login', query.getAccountName, query.validateAccountName);
 pageRouter.post('/signUp', query.createAccount);
 
